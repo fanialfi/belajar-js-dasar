77278cdcfb1af78564eff80c59c22536785ada34
fanialfi <fanialfirdaus@gmail.com>
2023-09-22 23:39:21 +0700

    beberapa tipe komentar pada javascript

---
+++ komentar/README.md
@@ -0,0 +1,3 @@
+# komentar
+
+komentar merupakan sebuah kode program yang tidak akan di eksekusi ketika program file dibaca.
---
+++ komentar/index.js
@@ -0,0 +1,11 @@
+// ini komentar satu baris
+console.log("hello World");
+document.writeln("Hello komentar");
+
+
+/*
+  ini komentar lebih dari satu baris,
+  kalau enggak percaya coba aja
+*/
+console.log("hello World");
+document.writeln("Hello komentar");
