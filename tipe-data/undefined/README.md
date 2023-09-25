92fa2fbe0f4c03c6108dafcbb4ad258f1e8f8410
fanialfi <fanialfirdaus@gmail.com>
2023-09-25 18:45:40 +0700

    tipe data undefined di js

---
+++ tipe-data/undefined/README.md
@@ -0,0 +1,3 @@
+undefined adalah sebuah keyword / tipe data di javascript.
+
+sebuah variabel yang belum ditambahkan nilainya, maka secara default isinya adalah undefined, ketika mengakses index yang tidak ada di array, maka juga akan mengembalikan undefined, ketika mengakses properti di object yang tidak ada, maka isinya juga undefined
---
+++ tipe-data/undefined/undefined.js
@@ -0,0 +1,34 @@
+let name;
+
+if (name == undefined) {
+  console.log(`undefined`);
+} else {
+  console.log(`define`);
+}
+
+let define = undefined;
+
+if (define == undefined) {
+  console.log(`undefined`);
+} else {
+  console.log(`defined`);
+}
+
+const nama = ["fani", "alfirdaus"];
+
+if (nama[2] == undefined) {
+  console.log(`array undefined`);
+} else {
+  console.log(`array defined`);
+}
+
+const person = {
+  "nama": "fani alfirdaus",
+  "alamat": "indonesia"
+}
+
+if (person.age == undefined) {
+  console.log(`age ${undefined}`);
+} else {
+  console.log(`defined`);
+}
