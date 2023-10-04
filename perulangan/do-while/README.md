096df34214b6e31b7801cda3dd3cbdc2241d6fc0
fanialfi <fanialfirdaus@gmail.com>
2023-10-04 08:37:47 +0700

    perulangan menggunakan do while

---
+++ perulangan/do-while/README.md
@@ -0,0 +1,5 @@
+# do-while loop
+
+perulangan ini mirip dengan `while` bedanya pengecekan kondisi di lakukan di akhir (dilakukan setelah perulangan sekali di jalankan)
+
+oleh karena itu perulangan `do while` akan di eksekusi minimal sekali.
---
+++ perulangan/do-while/do_while.js
@@ -0,0 +1,13 @@
+let counter = 11;
+
+while (counter <= 10) {
+  console.log(`isi counter adalah ${counter}`);
+  counter++
+}
+
+// minimal sekali di eksekusi
+do {
+  console.log(`isi counter adalah ${counter}`);
+  counter++
+} while (counter <= 10);
+
