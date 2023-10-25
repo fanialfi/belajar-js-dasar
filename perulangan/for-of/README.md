c4d562cc68fb4275b9235c889c002da2344ac89d
fanialfi <fanialfirdaus@gmail.com>
2023-10-25 21:29:54 +0700

    perulangan `for`-`in`

---
+++ perulangan/for-of/README.md
@@ -0,0 +1,4 @@
+`for`-`of` digunakan untuk mengiterasi terhadap isi value dari sebuah iterable object
+seperti array string dll.
+
+`for`-`of` tidak bisa digunakan pada sebuah object karena bukan sebuah iterable
---
+++ perulangan/for-of/for_of.js
@@ -0,0 +1,17 @@
+const name = ["fani", "alfirdaus"];
+for (const value of name) {
+  console.log(`${value}`);
+  console.log(` `);
+  for (const key of value) {
+    console.log(`${key}`);
+  }
+}
+
+const person = {
+  "namaDepan": "fani",
+  "namaBelakang": "alfirdaus",
+}
+
+for (const value of person) {
+  console.log(`${value}`);
+}
